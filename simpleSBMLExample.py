288e7cc90a52bf458c2cdce4ee380958d5d56853
Kiri Choi <kirichoi@uw.edu>
2016-02-19 10:00:08 -0800

    Fixed simpleSBML problem

--- simpleSBMLExample.py
+++ simpleSBMLExample.py
@@ -19,9 +19,9 @@ m.addParameter("k2", 0.2)
 m.addParameter("k3", 0.15)
 
 # Define reactions
-m.addReaction("[X0]", "[X1]", "k1*X0")
-m.addReaction("[X1]", "[X2]", "k2*X1")
-m.addReaction("[X2]", "[X3]", "k3*X2")
+m.addReaction(['X0'], ['X1'], "k1*X0")
+m.addReaction(['X1'], ['X2'], "k2*X1")
+m.addReaction(['X2'], ['X3'], "k3*X2")
 
 # Convert to SBML string
 sbmlStr = m.toSBML()
