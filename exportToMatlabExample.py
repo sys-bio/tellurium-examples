cc5533af21e7bac4db465c591e6e38ade9c09781
Kiri Choi <kirichoi@uw.edu>
2016-04-25 17:02:38 -0700

    Fix function name

--- exportToMatlabExample.py
+++ exportToMatlabExample.py
@@ -1,19 +1,21 @@
 """
 This script shows how to export models into executable MATLAB .m file.
-The script creates a file called 'model.m' to the same directory that
+The script creates a file called 'main.m' to the same directory that
 this script is located.
 """
 import tellurium as te
 import os.path
 
 r = te.loada('''
+model testmodel
     $Xo -> S1; vo;
     S1 -> S2; k1*S1 - k2*S2;
     S2 -> $X1; k3*S2;
     
     vo = 1
-    k1 = 2; k2 = 0; k3 = 3;
+    k1 = 2; k2 = 0; k3 = 3; 
+end
 ''')
 
 # Export model as a Matlab function
-r.exportToMatlab(os.path.join('.', 'model.m'))
+r.exportToMatlab(os.path.join('.', 'testmodel.m'))
